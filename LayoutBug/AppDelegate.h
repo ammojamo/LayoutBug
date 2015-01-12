88534e1e38a8de0c4399a87b5886377b1f3942e2
James Watmuff <james.watmuff@gmail.com>
2015-01-13 00:02:32 +1100

    Initial Commit

---
+++ LayoutBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutBug
+//
+//  Created by James Watmuff on 13/01/2015.
+//  Copyright (c) 2015 James Watmuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayoutBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutBug
+//
+//  Created by James Watmuff on 13/01/2015.
+//  Copyright (c) 2015 James Watmuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
